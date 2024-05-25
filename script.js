9efd1b0d146b9cee96a2bfcf5fc5989ea4076213
MarjoAnabel <marjorie_gonzaga97@hotmail.com>
2024-05-25 13:45:44 +0200

    primera fase

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const startbtn = document.getElementById('startbtn') //boton start
+const header = document.getElementById ('header') // encabezado
+const main = document.getElementById('main')// pagina principal
+const API_URL = 'https://opentdb.com/api.php?amount=10&difficulty=easy&type=boolean'
 
+function startgame() {
+    header.classList.remove('hide')
+    main.classList.add('hide')
+}  
+
+
+
+
+
+
+
+startbtn.addEventListener('click',startgame )
