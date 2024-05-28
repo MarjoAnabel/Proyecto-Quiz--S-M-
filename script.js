738c690ca272022ce350d7d447a3561feda17ac6
MarjoAnabel <marjorie_gonzaga97@hotmail.com>
2024-05-28 20:15:27 +0200

    on fire

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const answerButtonsElement = document.getElementById ('answer-buttons')
 function startgame() {
     header.classList.remove('hide')
     main.classList.add('hide')
-    setNextQuestion()
+    // setNextQuestion()
 } 
 
 let currentQuestionIndex
@@ -22,21 +22,53 @@ function startGame() {
     
 }
 
-startbtn.addEventListener ('click', () => {
-    const apiUrl = 'https://opentdb.com/api.php?amount=10&difficulty=easy&type=boolean'
-    axios.get(apiUrl)
-  .then(response => {
-    const questions = [];
-    response.data.results.forEach(result => {
-      questions.push(result.question);
-    });
-    console.log(questions);
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
+// // let dataGlobal = []
+// const getQuestions = () => {
+//   axios 
+// .get ('https://opentdb.com/api.php?amount=10&difficulty=easy&type=boolean')
+// .then ((res) => {
+//   const {data} = res
+// const getQuestion = () => {
+//   dataGlobal.forEach ((item) => {
+//     question.innerHTML += `
+//     <h2>${item.question} </h2>
+//     `
+//   })
+// }
+// })
+// .catch((err) => console.error (err))
+// }
 
-})
 
+function startGame() {
+  startbtn.classList.add ('hide')
+  questionElement.classList.remove ('hide')
+}
 
 startbtn.addEventListener('click',startGame)
+
+// document.getElementById('startbtn').addEventListener ('click',function() {
+//   document.getElementById ('container').style.display= 'none'
+
+// })
+
+
+
+// startbtn.addEventListener ('click', () => {
+//     const apiUrl = 'https://opentdb.com/api.php?amount=10&difficulty=easy&type=boolean'
+//     axios.get(apiUrl)
+//   .then(response => {
+//     let questions = [];
+//     response.data.results.forEach(result => {
+//             questions.innerHTML += `
+//             <h2>${result.question} </h2>
+//             `
+//           })
+//       // questions.push(result.question);
+//     });
+//     console.log(questions);
+//     return questions
+//   })
+//   .catch(error => {
+//     console.error('Error:', error);
+//   });
