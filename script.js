a23bbccd186b64f4626c15f0e545b07761e86a19
Sergio Mariño <sergiusmaf@gmail.com>
2024-05-28 20:21:09 +0200

    no hacer caso

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const startbtn = document.getElementById('startbtn') //boton start
-const header = document.getElementById ('header') // encabezado
+ const header = document.getElementById ('header') // encabezado
 const main = document.getElementById('main')// pagina principal
 const API_URL = 'https://opentdb.com/api.php?amount=10&difficulty=easy&type=boolean'
 
@@ -15,3 +15,16 @@ function startgame() {
 
 
 startbtn.addEventListener('click',startgame )
+
+const getPosts = () => {
+axios.get('https://opentdb.com/api.php?amount=10&difficulty=easy&type=boolean')
+.then((res) => { 
+  const {data} = res
+  data.forEach((item) => {
+    content.innerHTML += `
+    <h1>${item.title}</h1>
+    <p>${item.body}</p>`
+  })
+})
+.catch((err) => console.error(err))
+}
