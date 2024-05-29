71e33da331f1f6253b8e2afe60c33dc9e3d99fe6
MarjoAnabel <marjorie_gonzaga97@hotmail.com>
2024-05-29 12:08:39 +0200

    se muestra pregunta

--- script.js
+++ script.js
@@ -1,74 +1,71 @@
-// const apiUrl = 'https://opentdb.com/api.php?amount=10&difficulty=easy&type=boolean'
+// const apiUrl = 'https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple'
 const startbtn = document.getElementById('startbtn') //boton start
 const header = document.getElementById ('header') // encabezado
 const main = document.getElementById('main')// pagina principal
 const apiQuestion = document.getElementById ('api-question') //donde se alojan las preguntas
 const questionElement = document.getElementById ('question')
 const answerButtonsElement = document.getElementById ('answer-buttons')
-
-
-function startgame() {
-    header.classList.remove('hide')
-    main.classList.add('hide')
-    // setNextQuestion()
-} 
+const scoreElement = document.querySelector(".score");
 
 let currentQuestionIndex
 
+// Botón para comenzar el Quiz.
 function startGame() {
-    startbtn.classList.add('hide')
-    currentQuestionIndex = 0
-    questionElement.classList.remove('hide')
-    
+  // resetButton();
+  startbtn.classList.add("hide");
+
+  axios
+    .get("https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple")
+    .then((res) => {
+      questions = res.data.results;
+      currentQuestionIndex = 0;
+      questionElement.classList.remove("hide");
+      setNextQuestion();
+    })
+    .catch((err) => console.error(err));
+
+} 
+
+//Monstrar pregunta
+function showQuestion(questionObj) {
+  const pregunta = questionObj.question;
+  const preguntaObj = pregunta.replace(/&quot;/g, '"').replace(/&#039;/g, "'");
+
+  questionElement.innerText = preguntaObj;
+
+ }
+// Desestructuración del objeto para juntar las respuestas.
+const { correct_answer } = questionObj; //revisar
+const { incorrect_answers } = questionObj;
+const answers = [correct_answer, ...incorrect_answers];
+
+
+// Creación de botones con las respuestas.
+answers.forEach((answer) => {
+  const button = document.createElement("button");
+  button.innerHTML = answer;
+  button.classList.add("btn");
+  button.classList.add("btn-outline-primary");
+
+  // Mostrar pregunta siguiente.
+function setNextQuestion() {
+  // resetState();
+  showQuestion(questions[currentQuestionIndex]);
+
 }
 
-// // let dataGlobal = []
-// const getQuestions = () => {
-//   axios 
-// .get ('https://opentdb.com/api.php?amount=10&difficulty=easy&type=boolean')
-// .then ((res) => {
-//   const {data} = res
-// const getQuestion = () => {
-//   dataGlobal.forEach ((item) => {
-//     question.innerHTML += `
-//     <h2>${item.question} </h2>
-//     `
-//   })
-// }
-// })
-// .catch((err) => console.error (err))
-// }
 
 
-function startGame() {
-  startbtn.classList.add ('hide')
-  questionElement.classList.remove ('hide')
-}
 
+
+  // Setear como respuesta correcta si en el objeto es "correct_answer:".
+  if (answer == correct_answer) {
+    button.dataset.correct = true;
+  }
+
+})
+
+
+
+//Eventos 
 startbtn.addEventListener('click',startGame)
-
-// document.getElementById('startbtn').addEventListener ('click',function() {
-//   document.getElementById ('container').style.display= 'none'
-
-// })
-
-
-
-// startbtn.addEventListener ('click', () => {
-//     const apiUrl = 'https://opentdb.com/api.php?amount=10&difficulty=easy&type=boolean'
-//     axios.get(apiUrl)
-//   .then(response => {
-//     let questions = [];
-//     response.data.results.forEach(result => {
-//             questions.innerHTML += `
-//             <h2>${result.question} </h2>
-//             `
-//           })
-//       // questions.push(result.question);
-//     });
-//     console.log(questions);
-//     return questions
-//   })
-//   .catch(error => {
-//     console.error('Error:', error);
-//   });
