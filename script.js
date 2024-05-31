e131f6a118e063be00c4ba08fe6db8360cc9a4fc
MarjoAnabel <marjorie_gonzaga97@hotmail.com>
2024-05-31 20:30:04 +0200

    Proyecto finalizado

--- script.js
+++ script.js
@@ -151,24 +151,6 @@ nextbtn.addEventListener('click', () => {
 
 
 
-//Grafica
-// const correctAnswers = JSON.parse(localStorage.getItem('correctAnswers')) 
- 
-// const ctx = document.getElementById('myChart');
-// const labels = Utils.months({count: 7});
-// const data = {
-//   labels: labels,
-//   datasets: [{
-//     label: 'My First Dataset',
-//     data: [65, 59, 80, 81, 56, 55, 40],
-//     fill: false,
-//     borderColor: 'rgb(75, 192, 192)',
-//     tension: 0.1
-//   }]
-// };
-
-
-
 
 
 
